05be0b845db8766d5b126023cb49348ea06ed07c
Qaswar664 <99461247+Qaswar664@users.noreply.github.com>
2023-07-18 08:14:03 +0500

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,40 +2,46 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
+
 const Cart = () => {
-    const cartData = useSelector((state) => state.cartData);
-    let amount = cartData.length && cartData.map(item => item.price).reduce((prev, next) => prev + next)
-    console.warn(amount)
-    return (<div>
-       <Button className="btn btn-primary float-start m-1">
-       <Link className="text-decoration-none text-white" to="/" >Go Back</Link>
-       </Button>
-        
-        <h1>Cart Page</h1>
-        <div className="cart-page-container">
-            <table>
-                <tr>
-                    <th>Color</th>
-                    <th>Price</th>
-                    <th>Category</th>
-                </tr>
-                {
-                    cartData.map((item) => 
-                    <tr key={item.key}>
-                        <td>{item.category}</td>
-                        <td>{item.price}</td>
-                        <td>{item.category}</td>
-                    </tr>)
-                }
-            </table>
-            <div className="price-details">
-                <div className="adjust-price"><span>Amount</span><span>${amount}</span></div>
-                <div className="adjust-price"><span>Discount</span><span>${amount / 10}</span></div>
-                <div className="adjust-price"><span>Tax</span><span>$000</span></div>
-                <div className="adjust-price"><span>Total</span><span>${amount - (amount / 10)}</span></div>
-            </div>
+  const cartData = useSelector((state) => state.cartData);
+  let amount = cartData.length && cartData.map(item => item.price).reduce((prev, next) => prev + next)
+  
+  return (
+    <div>
+      <Button className="btn btn-primary float-start m-1">
+        <Link className="text-decoration-none text-white" to="/">Go Back</Link>
+      </Button>
+
+      <h1>Cart Page</h1>
+      <div className="cart-page-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartData.map((item) => (
+              <tr key={item.key}>
+                <td>{item.title.slice(0, 5)}</td>
+                <td>{item.price}</td>
+                <td>{item.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="price-details">
+          <div className="adjust-price text-success"><span>Amount</span><span>${amount}</span></div>
+          <div className="adjust-price text-danger"><span>Discount</span><span>${amount / 10}</span></div>
+          <div className="adjust-price"><span>Tax</span><span>$000</span></div>
+          <div className="adjust-price text-success"><span>Total</span><span>${amount - (amount / 10)}</span></div>
         </div>
-    </div>)
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
